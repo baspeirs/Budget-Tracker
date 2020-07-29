9d42c64581dbc0051bf701dc98dcc1d18723d82d
Bryce Speirs <baspeirs@gmail.com>
2020-07-29 10:42:15 -0400

    successfully creating and storing transactions when offline, and transactions being posted to DB when connection is re-established

--- public/index.js
+++ public/index.js
@@ -1,5 +1,6 @@
 let transactions = [];
 let myChart;
+let db;
 
 fetch("/api/transaction")
   .then(response => {
@@ -151,3 +152,70 @@ document.querySelector("#add-btn").onclick = function() {
 document.querySelector("#sub-btn").onclick = function() {
   sendTransaction(false);
 };
+
+// ========== INDEXED DB CODE ===============
+
+// create a new db request for a "budget" database.
+const request = window.indexedDB.open("budget", 1);
+
+request.onupgradeneeded = function(event) {
+// update the database variable with the database
+    db = event.target.result;
+
+// create object store called "pending" and set autoIncrement to true
+    const budgetStore = db.createObjectStore("pending", {
+      autoIncrement: true
+    });
+
+    budgetStore.createIndex("transaction", "transaction");
+};
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+    console.log(navigator)
+    if (navigator.onLine) {
+      checkDatabase();
+    }
+  };
+  
+request.onerror = function(event) {
+  // log error here
+  console.log(event.target.errorCode)
+};
+
+function saveRecord(record) {
+  console.log(record);
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+
+  store.add(record);
+}
+
+function checkDatabase() {
+  // open a transaction on your pending db
+  const transaction = db.transaction(["pending"], "readwrite");
+  // access your pending object store
+  const store = transaction.objectStore("pending");
+  // get all records from store and set to a variable
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function() {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      })
+      .then(response => response.json())
+      .then(() => {
+        store.clear();
+      });
+    }
+  };
+}
+
+// listen for app coming back online
+window.addEventListener("online", checkDatabase);
