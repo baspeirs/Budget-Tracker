070d3bb3a239162a3e8d3712c8732caac6b01751
Bryce Speirs <baspeirs@gmail.com>
2020-07-29 10:58:31 -0400

    updated env variable for deploying mongoose application

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const PORT = 3000;
 
@@ -15,10 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(MONGODB_URI);
 
 // routes
 app.use(require("./routes/api.js"));
